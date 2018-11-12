49d69aa186d875b9c487c1346b01266217e3a9ff
densiteb <krittin13@gmail.com>
2018-11-12 23:25:41 +0700

    first

---
+++ django/mysite/api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Note(models.Model):
+    Value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return '%s %s' % (self.Value,self.created_at)
+
+# Create your models here.
