eea273e5952d72bece22e95ff69d145a339f2f14
densiteb <krittin13@gmail.com>
2018-11-27 00:05:14 +0700

    Vue+django test

--- django/mysite/api/pyrebase_settings.py
+++
@@ -1,9 +0,0 @@
-import pyrebase
-config = {
-    "apiKey" : "AIzaSyCb8xRzfsic8CRqdRr1FBdnSfPrnbg_nIc",
-    "authDomain" : "softdev18-a2b9c.firebaseapp.com",
-    "databaseURL" : "https://softdev18-a2b9c.firebaseio.com",
-    "storageBucket" : "softdev18-a2b9c.appspot.com"
-}
-
-firebase = pyrebase.initialize_app(config)
--- django/mysite/polls/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import questions
-admin.site.register(questions)
-# Register your models here.
